b089fd6a40f9f43723401d39e31c425b2f8f2706
7-Stack <ashleyculture@yahoo.com>
2023-08-17 15:14:41 +0100

    Modified Route Message

--- src/app.routes/index.routes.js
+++ src/app.routes/index.routes.js
@@ -4,13 +4,13 @@ const basePath = "/api/v1";
 
 module.exports = (app) => {
     app.get(`${basePath}/docs`, (req, res) => {
-        res.redirect()
+        res.redirect("https://documenter.getpostman.com/view/28028237/2s9Y5R2mK5")
     })
 
     app.get(`${basePath}/health-check`, (req, res) => {
         res.send({
             success: true,
-            message: "Health Check"
+            message: "Server is Live!"
         })
     })
 };
