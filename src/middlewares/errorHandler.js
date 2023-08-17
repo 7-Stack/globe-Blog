0e1ce2df8bfa14d448218e7e2737da03bccc501c
7-Stack <ashleyculture@yahoo.com>
2023-08-17 15:18:05 +0100

    File was Formatted

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
     const env = process.env.NODE_ENV
     const stackTrace = env !== "development" ? undefined : err.stack
     const statusCode = res.statusCode ? res.statusCode : 500;
-    switch (statusCode){
+    switch (statusCode) {
         case constants.VALIDATION_ERROR:
             res.send({
                 title: "Validation Failed",
@@ -11,36 +11,36 @@ const errorHandler = (err, req, res, next) => {
                 stackTrace
             });
             break;
-            case constants.NOT_FOUND:
-                res.send({
-                    title: "Not Found",
-                    message: err.messsage,
-                    stackTrace
-                });
-                case constants.UNAUTHORIZED:
-                    res.send({
-                        title: "Unauthorized",
-                        messgae: err.message,
-                        stackTrace
-                    });
-                    case constants.FORBIDDEN:
-                        res.send({
-                            title: "Forbidden",
-                            message: err.message,
-                            stackTrace
-                        });
-                        case constants.SERVER_ERROR:
-                            res.send({
-                                title: "Server Error",
-                                message: err.message,
-                                stackTrace
-                            });
-                            default:
+        case constants.NOT_FOUND:
+            res.send({
+                title: "Not Found",
+                message: err.messsage,
+                stackTrace
+            });
+        case constants.UNAUTHORIZED:
+            res.send({
+                title: "Unauthorized",
+                messgae: err.message,
+                stackTrace
+            });
+        case constants.FORBIDDEN:
+            res.send({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace
+            });
+        case constants.SERVER_ERROR:
+            res.send({
+                title: "Server Error",
+                message: err.message,
+                stackTrace
+            });
+        default:
 
-                            return res.status(500).send({
-                                success: false,
-                                message: err.message
-                            });
+            return res.status(500).send({
+                success: false,
+                message: err.message
+            });
     }
 };
 
