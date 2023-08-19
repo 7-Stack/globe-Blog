30a1fe2408fab01791b480ca0cfadfafe9d720b1
7-Stack <ashleyculture@yahoo.com>
2023-08-19 12:12:40 +0100

    Resolved Merge Conflict

---
+++ src/app.routes/index.routes.js
@@ -0,0 +1,18 @@
+const app = require("../../app");
+const UserRoute = require("../users/users.routes");
+
+const basePath = "/api/v1";
+
+module.exports = (app) => {
+    app.use(`${basePath}/users`, UserRoute);
+    app.get(`${basePath}/docs`, (req, res) => {
+        res.redirect("https://documenter.getpostman.com/view/28028237/2s9Y5R2mK5")
+    })
+
+    app.get(`${basePath}/health-check`, (req, res) => {
+        res.send({
+            success: true,
+            message: "Server is Live!"
+        })
+    })
+};
