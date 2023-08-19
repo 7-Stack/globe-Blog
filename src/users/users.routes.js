534bb188fb3ec97e37f2c69d3cc509191e8088ba
7-Stack <ashleyculture@yahoo.com>
2023-08-19 12:18:38 +0100

    User Sign-up Routes

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -4,10 +4,11 @@ const userController = require("./users.controller");
 const UserRoute = express.Router();
 
 UserRoute.post("/", userController.create);
+// UserRoute.get("/:id", userController.get);
 
-// UserRoute.get("/me", [
-//     authenticate
-// ], userController.me);
+UserRoute.get("/me", [
+    // authenticate
+], userController.me);
 
 
 
