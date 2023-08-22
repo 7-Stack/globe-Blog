0fff0d621e6fff9120482a8dcc3a02b6c628e0e0
7-Stack <ashleyculture@yahoo.com>
2023-08-22 14:59:23 +0100

    Implemented Get Routes

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const userController = require("./users.controller");
-
 const UserRoute = express.Router();
+const validator = require("../middlewares/validator.middleware");
+const { CreateUserSchema } = require("../users/users.schema-validator");
+// const { router } = require("../../app");
+
+UserRoute.route("/")
+.get(userController.getUsers)
+.post([validator(CreateUserSchema)], userController.create);
+
+UserRoute.route("/:id")
+.get(userController.getUser)
+// .put(userController.updateUser)
+// .delete(userController.deleteUser);
 
 UserRoute.post("/", userController.create);
-// UserRoute.get("/:id", userController.get);
-
 UserRoute.get("/me", [
     // authenticate
 ], userController.me);
+
 
 
 
