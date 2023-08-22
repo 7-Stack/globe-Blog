d4ad2a2c71403d0f56068939d620991a3427b193
7-Stack <ashleyculture@yahoo.com>
2023-08-22 14:52:08 +0100

    Middleware Validator

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,15 @@
+// const { schema } = require("../users/users.model");
+
+const validate = (schema) => async(req, res, next) => {
+    try {
+        const value = await schema.validateAsync(req.body);
+        next();
+    } catch(error) {
+        return res.status(res.statusCode || 400).send({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+module.exports = validate
