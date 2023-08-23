7aa741437190618dba566084992aa44791c2a723
Ashley Ifesinachi <133854024+7-Stack@users.noreply.github.com>
2023-08-23 09:40:52 +0100

    Feat/user sign up (#3)
    * Port Middleware
    
    * Error Middlewares
    
    * Global Route
    
    * Global Error Responses
    
    * Script was Modified
    
    * Modified Route Message
    
    * File was Formatted
    
    * Deleted File
    
    * User-Sign-up-Logics
    
    * Resolved Merge Conflict
    
    * Modified Sign-up Logic
    
    * User Sign-up Routes
    
    * Middleware Validator
    
    * Added Read Methods
    
    * Implemented Get Routes
    
    * Joi Validator for Create
    
    * Modified File Name
    
    * Modified a Schema Repetition
    
    * Modified the Validator Schema

--- src/app.routes/index.routes.js
+++ src/app.routes/index.routes.js
@@ -1,8 +1,10 @@
 const app = require("../../app");
+const UserRoute = require("../users/users.routes");
 
 const basePath = "/api/v1";
 
 module.exports = (app) => {
+    app.use(`${basePath}/users`, UserRoute);
     app.get(`${basePath}/docs`, (req, res) => {
         res.redirect("https://documenter.getpostman.com/view/28028237/2s9Y5R2mK5")
     })
---
+++ src/users/users.controller.js
@@ -0,0 +1,49 @@
+const User = require("../users/users.model");
+const userService = require("./users.service");
+const { CreateUserSchema } = require("../users/users.schema-validator");
+class UserController {
+    async create(req, res) {
+        // check if user exists
+        const existingUser = await userService.findOne({ email: req.body.email })
+        if (existingUser) {
+            return res.status(409).send({ success: false, message: "User already exists" })
+        }
+
+        // create the user
+        // const value = await CreateUserSchema.validateAsync(req.body);
+        UserService.create(req.body)
+        const user = await userService.create(value);
+        return res.status(201).send({ success: true, message: "User created successfully", data: user })
+    };
+
+    async me(req, res) {
+        const user = await userService.findOne({
+            _id: req.user?._id
+        })
+
+        return res.status(200).send({
+            success: true,
+            message: "User retrieved on request",
+            data: user
+        })
+    };
+
+    async getUsers (req, res) {
+        console.log("The request body is");
+        const users = await User.find();
+        res.status(200).json(users);
+    };
+
+    async getUser(req, res) {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            res.status(404);
+            throw new Error("User not found");
+        }
+        res.status(201).json(user);
+    };
+
+}
+
+
+module.exports = new UserController()
