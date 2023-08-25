206b1ae60d21abe20cc024c8ce2ad73dcb2196ff
7-Stack <ashleyculture@yahoo.com>
2023-08-25 16:36:06 +0100

    Post Schema

--- src/posts/posts.schema-validator.js
+++ src/posts/posts.schema-validator.js
@@ -3,9 +3,21 @@ const Joi = require("joi");
 exports.CreatePostSchema = Joi.object({
     title: Joi.string()
     .min(3)
-    .max(100).require(),
+    .max(100)
+    .required(),
 
     content: Joi.string().min(70).max(1500),
 
     Image: Joi.string()
+})
+
+exports.UpdatePostSchema = Joi.object({
+    title: Joi.string()
+    .min(3)
+    .max(100)
+    .required(),
+
+    content: Joi.string().min(30).max(1500),
+
+    // Image: Joi.string()
 })
