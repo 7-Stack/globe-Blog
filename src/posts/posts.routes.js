f2aa3c889ba24e08cfd563ea94b72406cb6fe59f
7-Stack <ashleyculture@yahoo.com>
2023-08-30 12:00:41 +0100

    All Routes Call Controller

--- src/posts/posts.routes.js
+++ src/posts/posts.routes.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const PostRoute = express.Router();
 const postController = require("./posts.controller");
-const postService = require("./posts.service");
 const validator = require("../middlewares/validator.middleware");
 const { CreatePostSchema } = require("./posts.schema-validator");
 const { UpdatePostSchema } = require("./posts.schema-validator");
 
 PostRoute.route("/")
-.get(postService.getPosts)
+.get(postController.getPosts)
 .post([validator(CreatePostSchema)], postController.createPost);
 
 
 PostRoute.route("/:id")
-.get(postService.getPost)
+.get(postController.getPost)
 .put([validator(UpdatePostSchema)], postController.updatePost)
-.delete(postService.deletePost);
+.delete(postController.deletePost);
 
 
 module.exports = PostRoute;
