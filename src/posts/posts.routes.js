11a2f6568f8538473a9e8fa194983267ff4f5ebb
7-Stack <ashleyculture@yahoo.com>
2023-09-13 14:09:32 +0100

    Added Image Upload Routes

--- src/posts/posts.routes.js
+++ src/posts/posts.routes.js
@@ -1,19 +1,27 @@
 const express = require("express");
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const PostRoute = express.Router();
 const postController = require("./posts.controller");
 const validator = require("../middlewares/validator.middleware");
+
 const { CreatePostSchema } = require("./posts.schema-validator");
 const { UpdatePostSchema } = require("./posts.schema-validator");
 
 PostRoute.route("/")
 .get(postController.getPosts)
-.post([validator(CreatePostSchema)], postController.createPost);
-
+.post([upload.single('image'),validator(CreatePostSchema)], postController.createPost);
 
 PostRoute.route("/:id")
 .get(postController.getPost)
 .put([validator(UpdatePostSchema)], postController.updatePost)
 .delete(postController.deletePost);
+
+PostRoute.put('/:id/upload',
+ postController.uploadImage
+)
 
 
 module.exports = PostRoute;
